7389865d3f0535861682f1cac3767b845918df82
kered <wainam13@yahoo.com.sg>
2021-12-11 17:05:16 +0800

    2nd commit

---
+++ python/spyder.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 11 16:22:45 2021
+
+@author: Kered
+"""
+
+'''
+Assignment 1 part 3
+'''
+# print integers 1,2,.....,10
+print (1,2,3,4,5,6,7,8,9,10)
